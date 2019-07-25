ce37d3a371b66b45fa8613f037d8555b162b272f
JWHAPO <kim2101714@gmail.com>
2019-07-25 23:10:46 +0900

    add debugshowcheckedModeBanner - false

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Recipe'),
     );
   }
