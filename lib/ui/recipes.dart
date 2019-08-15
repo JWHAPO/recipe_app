3298d9cb3da87f44aecf7a5e0f0cbce7f9846240
JWHAPO <kim2101714@gmail.com>
2019-08-15 22:37:39 +0900

    .

--- lib/ui/recipes.dart
+++ lib/ui/recipes.dart
@@ -86,7 +86,7 @@ class _RecipePageState extends State<RecipePage> {
                     hintText: "Search",
                     prefixIcon: Icon(Icons.search),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0))
+                      borderRadius: BorderRadius.all(Radius.circular(12.0))
                     )
                   ),
                 ),
