6871e7a289e92d30e738716b9ec450c836c0ba11
JWHAPO <kim2101714@gmail.com>
2019-08-19 15:10:30 +0900

    add gridview with bloc

--- lib/model/recipe.dart
+++ lib/model/recipe.dart
@@ -4,13 +4,15 @@ class Recipe{
   String subTitle;
   String contents;
   String time;
+  String imageUrl;
 
-  Recipe({this.id, this.title, this.subTitle, this.contents, this.time});
+  Recipe({this.id, this.title, this.subTitle, this.contents, this.time, this.imageUrl});
 
   Recipe.fromJson(Map json)
     :id = json['id'],
     title = json['title'], //레시피명
     subTitle = json['subTitle'],//부제목
     contents = json['contents'],//내용
-    time = json['time'];//조리시간
+    time = json['time'],
+    imageUrl = json['imageUrl'];//조리시간
 }
--- lib/ui/favorites.dart
+++ lib/ui/favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/bloc/recipes/recipes_provider.dart';
 
 class FavoritesPage extends StatefulWidget {
   FavoritesPage();
@@ -9,81 +10,39 @@ class FavoritesPage extends StatefulWidget {
 class _FavoritesPageState extends State<FavoritesPage> {
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      children: List.generate(choices.length, (index){
-        return Center(
-          child: ChoiceCard(choice: choices[index],),
-        );
-      }),
+
+    final recipeBloc = RecipesProvider.of(context);
+    recipeBloc.getRecipes();
+
+    return StreamBuilder(
+      stream: recipeBloc.results,
+      builder: (context, snapshot){
+        if(!snapshot.hasData){
+          return Text('No Data');
+        }else if(snapshot.hasError){
+          return Text('Occured Error');
+        }else{
+          return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+              itemBuilder: (context, index) => makeCard(snapshot, index)
+          );
+        }
+      },
     );
   }
 
-
+Card makeCard(AsyncSnapshot snapshot, int index) => Card(
+  color: Colors.white,
+  child: Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Image.network(snapshot.data[index].imageUrl,width: 60.0, height: 60.0,),
+        Text(snapshot.data[index].title, style: TextStyle(fontFamily: 'iropke'),)
+      ],
+    ),
+  ),
+);
 
 }
-
-class ChoiceCard extends StatelessWidget {
-  const ChoiceCard({Key key, this.choice}) : super(key: key);
-  final Choice choice;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return Card(
-        color: Colors.white,
-        child: Center(child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Icon(choice.icon, size:60.0, color: textStyle.color),
-              Text(choice.title, style: textStyle),
-            ]
-        ),
-        )
-    );
-  }
-}
-
-class Choice {
-  const Choice({this.title, this.icon});
-
-  final String title;
-  final IconData icon;
-}
-
-const List<Choice> choices = const <Choice>[
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
-  const Choice(title: 'Boat', icon: Icons.directions_boat),
-  const Choice(title: 'Bus', icon: Icons.directions_bus),
-  const Choice(title: 'Train', icon: Icons.directions_railway),
-  const Choice(title: 'Walk', icon: Icons.directions_walk),
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.drafts),
-  const Choice(title: 'Boat', icon: Icons.dvr),
-  const Choice(title: 'Bus', icon: Icons.copyright),
-  const Choice(title: 'Train', icon: Icons.cloud_off),
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
-  const Choice(title: 'Boat', icon: Icons.directions_boat),
-  const Choice(title: 'Bus', icon: Icons.directions_bus),
-  const Choice(title: 'Train', icon: Icons.directions_railway),
-  const Choice(title: 'Walk', icon: Icons.directions_walk),
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.drafts),
-  const Choice(title: 'Boat', icon: Icons.dvr),
-  const Choice(title: 'Bus', icon: Icons.copyright),
-  const Choice(title: 'Train', icon: Icons.cloud_off),
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
-  const Choice(title: 'Boat', icon: Icons.directions_boat),
-  const Choice(title: 'Bus', icon: Icons.directions_bus),
-  const Choice(title: 'Train', icon: Icons.directions_railway),
-  const Choice(title: 'Walk', icon: Icons.directions_walk),
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.drafts),
-  const Choice(title: 'Boat', icon: Icons.dvr),
-  const Choice(title: 'Bus', icon: Icons.copyright),
-  const Choice(title: 'Train', icon: Icons.cloud_off),
-];
