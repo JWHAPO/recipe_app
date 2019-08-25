85957c32d012a108d40c6fd3d218bd273a50df1b
JWHAPO <kim2101714@gmail.com>
2019-08-25 15:11:13 +0900

    trim create recipe page

--- lib/ui/create.dart
+++ lib/ui/create.dart
@@ -11,8 +11,29 @@ class CreateRecipe extends StatefulWidget {
 class _CreateRecipeState extends State<CreateRecipe> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('create page'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Color.fromRGBO(255, 242, 230, 1.0)),
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0.1,
+          backgroundColor: Color.fromRGBO(255, 242, 230, 1.0),
+          title: Text('Create recipe'),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: (){
+                  print('close button clicked');
+                  Navigator.of(context).pop();
+                  
+                },
+                child: Text('Close'),
+                shape: CircleBorder(side: BorderSide(color: Colors.transparent)),),
+          ],
+        ),
+        body: Center(
+          child: Text('Create Page'),
+        ),
+      ),
     );
   }
 }
