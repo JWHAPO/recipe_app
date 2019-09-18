6441892daf483070540623a1b083afb8e6058a26
JWHAPO <kim2101714@gmail.com>
2019-09-18 19:06:38 +0900

    add horizontal listview on thierview

--- lib/ui/create.dart
+++ lib/ui/create.dart
@@ -179,12 +179,34 @@ class _CreateRecipeState extends State<CreateRecipe> {
     var thirdBody = Column(
       children: <Widget>[
         Text('Select type'),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('This is third'),
-
-          ],
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
         )
       ],
     );
