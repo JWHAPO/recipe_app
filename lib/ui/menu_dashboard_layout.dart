c08f9b4d71d2bce4ee09d462f5351a1a98f52b58
JWHAPO <kim2101714@gmail.com>
2019-10-01 00:14:16 +0900

    add menu click animation

--- lib/ui/menu_dashboard_layout.dart
+++ lib/ui/menu_dashboard_layout.dart
@@ -8,11 +8,35 @@ class ManeDashboardPage extends StatefulWidget {
   _ManeDashboardPageState createState() => _ManeDashboardPageState();
 }
 
-class _ManeDashboardPageState extends State<ManeDashboardPage> {
+class _ManeDashboardPageState extends State<ManeDashboardPage> with SingleTickerProviderStateMixin{
   bool isCollapsed = true;
+  double screenWidth, screenHeight;
+  final Duration duration = const Duration(milliseconds: 300);
+  AnimationController _controller;
+  Animation<double> _scaleAnimation;
+  Animation<double> _menuScaleAnimation;
+  Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: duration);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.6).animate(_controller);
+    _menuScaleAnimation = Tween<double>(begin: 0.5, end: 1).animate(_controller);
+    _slideAnimation = Tween<Offset>(begin: Offset(-1,0), end: Offset(0,0)).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
     return Scaffold(
       backgroundColor: backgroundColor,
       body: Stack(
@@ -24,48 +48,98 @@ class _ManeDashboardPageState extends State<ManeDashboardPage> {
   }
 
   Widget menu(context){
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Dashboard', style: TextStyle(color: Colors.white, fontSize: 20),),
-            SizedBox(height: 10,),
-            Text('Messages', style: TextStyle(color: Colors.white, fontSize: 20),),
-            SizedBox(height: 10,),
-            Text('Utility Bills', style: TextStyle(color: Colors.white, fontSize: 20),),
-            SizedBox(height: 10,),
-            Text('Funds Trasfer', style: TextStyle(color: Colors.white, fontSize: 20),),
-            SizedBox(height: 10,),
-            Text('Branches', style: TextStyle(color: Colors.white, fontSize: 20),)
-          ],
+    return SlideTransition(
+      position: _slideAnimation,
+      child: ScaleTransition(
+        scale: _menuScaleAnimation,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Dashboard', style: TextStyle(color: Colors.white, fontSize: 20),),
+                SizedBox(height: 10,),
+                Text('Messages', style: TextStyle(color: Colors.white, fontSize: 20),),
+                SizedBox(height: 10,),
+                Text('Utility Bills', style: TextStyle(color: Colors.white, fontSize: 20),),
+                SizedBox(height: 10,),
+                Text('Funds Trasfer', style: TextStyle(color: Colors.white, fontSize: 20),),
+                SizedBox(height: 10,),
+                Text('Branches', style: TextStyle(color: Colors.white, fontSize: 20),)
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
   Widget dashboard(context){
-    return Material(
-      elevation: 8,
-      color: backgroundColor,
-      child: Container(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
+    return AnimatedPositioned(
+      duration: duration,
+      top: 0,
+      bottom: 0,
+      left: isCollapsed ? 0 : 0.6*screenWidth,
+      right: isCollapsed ? 0 : -0.4*screenWidth,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Material(
+          animationDuration: duration,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          elevation: 8,
+          color: backgroundColor,
+          child: Container(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
+            child: Column(
               children: <Widget>[
-                Icon(Icons.menu, color: Colors.white,),
-                Text('My Card', style: TextStyle(fontSize: 24, color: Colors.white),),
-                Icon(Icons.settings, color: Colors.white,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    InkWell(child: Icon(Icons.menu, color: Colors.white,), onTap: (){
+                      setState(() {
+                        if(isCollapsed) _controller.forward();
+                        else _controller.reverse();
+                        isCollapsed = !isCollapsed;
+                      });
+                    },),
+                    Text('My Card', style: TextStyle(fontSize: 24, color: Colors.white),),
+                    Icon(Icons.settings, color: Colors.white,),
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Container(
+                  height: 200,
+                  child: PageView(
+                    controller: PageController(viewportFraction: 0.8),
+                    scrollDirection: Axis.horizontal,
+                    pageSnapping: true,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        color: Colors.redAccent,
+                        width: 100,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        color: Colors.blueAccent,
+                        width: 100,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        color: Colors.greenAccent,
+                        width: 100,
+                      )
+                    ],
+                  ),
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
